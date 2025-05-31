1422cc2325334f357fd5079f54e4ad59737ad148
Devil666coin <evilmonkeybscz@gmail.com>
2025-05-31 12:42:20 +0100

    Fix parsing API_ID and CHANNEL_ID

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID_STR = os.getenv("CHANNEL_ID")
 
 # Controlli di esistenza
-if not all([API_ID_STR, API_HASH, BOT_TOKEN, CHANNEL_ID_STR]):
+if not all([BOT_TOKEN, CHANNEL_ID_STR, API_ID_STR, API_HASH]):
     raise ValueError("❌ Una o più variabili di ambiente mancano nelle env di Railway")
 
 try:
     API_ID = int(API_ID_STR)
     CHANNEL_ID = int(CHANNEL_ID_STR)
-except ValueError:
-    raise ValueError("❌ API_ID o CHANNEL_ID non sono numeri validi (devono essere int)")
+except Exception:
+    raise ValueError("❌ API_ID o CHANNEL_ID non sono numeri validi (devono essere int)"
 
 # File di storage
 MENTIONS_FILE = "mentions.json"
